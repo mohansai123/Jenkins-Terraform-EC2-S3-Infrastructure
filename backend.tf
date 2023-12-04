e6c0dd1143c75c5f8beafb15b75b4dd48e8ef1b4
mohansai123 <mohansai3333@gmail.com>
2023-12-04 14:53:38 +0530

    files were added

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "jenkins-pipeline-ec2"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "project-1table"
-  }
-}
