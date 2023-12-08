7b6e6b6fcc7e7cefcb31de4cfae0b77de621284e
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2023-12-08 18:15:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "project1-s3-bucket-1"
-    key            = "terraform.tfstate"
+    key            = "public-cli-keypair"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "project-1table"
