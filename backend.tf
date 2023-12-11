dec99237f161946213519ac234caa3427c1893d3
mohansai123 <mohansai3333@gmail.com>
2023-12-11 16:27:54 +0530

    files were created

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "project1-s3-bucket-1"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "s3-table-1"
-  }
-}
