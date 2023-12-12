b624dce4a3b7b86192652ff68381146316ce30ba
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2023-12-12 11:00:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "availability_zone" {
 
 variable "iam_instance_profile" {
   description = "EC2 instance IAM instance profile name"
-  default     = "github-ec2 "
+  default     = "ec2-permissons"
 }
 
 variable "key_name" {
